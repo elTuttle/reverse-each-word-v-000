b5912f44f321f3ff02cf6339992833966864cfdf
John Tuttle <thejwt@gmail.com>
2017-09-26 01:48:46 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
 def reverse_each_word(phrase)
   phrase_array = phrase.split(" ")
+  phrase_array.collect do |word_reverse|
+    word_reverse = word_reverse.reverse
+  end
+  phrase = phrase_array.join(" ")
+  phrase
 end
